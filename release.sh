36e9baf6f79b17ace64892347dc6c6dfa5ebdf1e
realmatic <admin@realmatic.com>
2023-08-05 12:54:41 -0700

    reorg

---
+++ release.sh
@@ -0,0 +1,5 @@
+# wget --no-check-certificate -O index.html https://julia.realmatic.local/
+# wget --no-check-certificate -O page/projects.html https://julia.realmatic.local/page/projects
+# wget --no-check-certificate -O page/musings.html https://julia.realmatic.local/page/musings
+# wget --no-check-certificate -O page/biocv.html https://julia.realmatic.local/page/biocv
+# wget --no-check-certificate -O page/230130-origami.html https://julia.realmatic.local/page/230130-origami
