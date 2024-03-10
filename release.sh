fe6a95f67dd3fbbb09f8d7b8ccce18fc2d1669e7
realmatic <admin@realmatic.com>
2024-03-09 18:00:05 -0800

    capstone

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
-# wget --no-check-certificate -O index.html https://julia.realmatic.local/
-# wget --no-check-certificate -O page/projects.html https://julia.realmatic.local/page/projects
-# wget --no-check-certificate -O page/musings.html https://julia.realmatic.local/page/musings
-# wget --no-check-certificate -O page/biocv.html https://julia.realmatic.local/page/biocv
-# wget --no-check-certificate -O page/230130-origami.html https://julia.realmatic.local/page/230130-origami
+# set DEBUG false, production ASSET URL
+
+# wget --no-check-certificate -O index.html $JULIA 
+# wget --no-check-certificate -O page/projects.html $JULIA/page/projects
+# wget --no-check-certificate -O page/musings.html $JULIA/page/musings
+# wget --no-check-certificate -O page/biocv.html $JULIA/page/biocv
+# wget --no-check-certificate -O page/230130-origami.html $JULIA/page/230130-origami
